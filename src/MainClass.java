020d0a3b96eb7c1120d0c0c91153793d5b58105b
maffeiandrea <maf.and@libero.it>
2019-10-23 15:41:59 +0200

    Vediamo se cancella il file MainClass

--- src/MainClass.java
+++
@@ -1,9 +0,0 @@
-
-public class MainClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-         System.out.println("sono la prima sysout da trasferire su git e github");
-	}
-
-}
---
+++ src/it/aci/sn/command/DisplayJava.java
@@ -0,0 +1,5 @@
+package it.aci.sn.command;
+
+public class DisplayJava {
+
+}
