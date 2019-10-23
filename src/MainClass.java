f43e749246329bbf473316e268574a6aabd6ca69
maffeiandrea <maf.and@libero.it>
2019-10-23 11:18:44 +0200

    inserisco la prima riga sysout per trasferirla su gir e git hub

---
+++ src/MainClass.java
@@ -0,0 +1,9 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("sono la prima sysout da trasferire su git e github");
+	}
+
+}
