be4e2ad08a4a9853b300eba77047de021bc9c831
maffeiandrea <maf.and@libero.it>
2019-10-23 11:23:16 +0200

    Corretto il nome pckage che non era staato impostatato.

---
+++ src/it/aci/sn/command/MainClass.java
@@ -0,0 +1,12 @@
+package it.aci.sn.command;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("sono la prima sysout da trasferire su git e github");
+         System.out.println("ho crretto il package e ho rimesso la classe che contiene : sono la prima sysout da trasferire su git e github");
+         
+	}
+
+}
