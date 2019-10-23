9c6bea7171238ce506697a8529e8155346445bae
maffeiandrea <maf.and@libero.it>
2019-10-23 16:16:18 +0200

    Ho aggiunto display da workspece 2 ed eliminato una display

--- src/it/aci/sn/command/MainClass.java
+++ src/it/aci/sn/command/MainClass.java
@@ -6,7 +6,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
          System.out.println("sono la prima sysout da trasferire su git e github");
          System.out.println("ho crretto il package e ho rimesso la classe che contiene : sono la prima sysout da trasferire su git e github");
-         System.out.println("viaggiare con git");
+         System.out.println("inserisco rig da workspace 2 ed elimino riga viaggiare con git");
 	}
 
 }
